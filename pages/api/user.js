d4d09c70cf81c6d2819eb42cbbf086a88e7bfb7a
Felipe Barreto <felipe.barreto@osf.digital>
2022-08-08 15:32:23 -0300

    users endpoint

---
+++ pages/api/user.js
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+/**
+ * @swagger
+ *
+ * /api/user:
+ *   get:
+ *     summary: Get user by email
+ *     tags:
+ *       - Users
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User email
+ *     responses:
+ *       200:
+ *         description: returns the user identified by the email.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *               example:   # Sample object
+ *                 id: 0
+ *                 name: "User Name"
+ *                 email: "user.name@email.com"
+ */
+export default function user(req, res) {
+  if (req.method === "GET") {
+    return getUserByEmail(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false });
+  }
+}
+
+async function getUserByEmail(req, res) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: req.query.email,
+      },
+    });
+
+    if (user) {
+      return res.status(200).json(user, { success: true });
+    }
+    res
+      .status(400)
+      .json({
+        error: "Could not found user with specified email",
+        success: false,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error retrieving the user", success: false });
+  }
+}
